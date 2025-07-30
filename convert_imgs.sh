a0a1a55544ab7c93910371936142c9801ea15258
christophergarthwood <christopher.g.wood@gmail.com>
2025-07-30 13:31:55 -0500

    Added scripts to convert cheatsheets.

---
+++ convert_imgs.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+#sudo apt install imagemagick
+export cmd_convert="/usr/bin/convert";
+
+img_extensions=( JPEG JPG GIF TIFF TIF )
+export target_extension="png"
+
+for ext in ${img_extensions[@]}
+do
+    actual_ext=$( echo ${ext} | tr '[:upper:]' '[:lower:]' )
+    echo "Processing extension ending with ${actual_ext}";
+    file_array=( $(find ./ -name "*.${actual_ext}" -print )  )
+    for file in ${file_array[@]}
+    do
+        echo "......processing ${file}";
+        export just_the_dirname=$(dirname ${file});
+        export just_the_filename=$(basename ${file});
+        export just_the_filename="${just_the_filename%.*}";
+        echo ".........${cmd_convert} ${file} ${just_the_dirname}/${just_the_filename}.${target_extension}";
+        ${cmd_convert} "${file}" "${just_the_dirname}/${just_the_filename}.${target_extension}";
+        status=$?
+        if [ ${status} -ne 0 ];
+        then
+            echo "WARNING: convert on ${file} resulted in failure with status code of: ${status}";
+        fi
+    done
+done
